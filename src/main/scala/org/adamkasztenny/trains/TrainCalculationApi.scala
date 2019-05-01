91ba2ffe78be70e6fe6a108ac8f16b508ac6b657
Adam Kasztenny <adamkasztenny@gmail.com>
2019-05-01 00:11:03 -0400

    API method for shortest path between cities and tests for it

--- src/main/scala/org/adamkasztenny/trains/TrainCalculationApi.scala
+++ src/main/scala/org/adamkasztenny/trains/TrainCalculationApi.scala
@@ -1,13 +1,21 @@
 package org.adamkasztenny.trains
 
 import org.adamkasztenny.trains.graph.TrainGraphTypes.TrainGraph
-import org.adamkasztenny.trains.graph.calculation.DistanceCalculator
+import org.adamkasztenny.trains.graph.calculation.{DistanceCalculator, ShortestPathCalculator}
 
 class TrainCalculationApi(graph: TrainGraph) {
+
+  val NoRouteMessage: String = "NO SUCH ROUTE"
 
   def distanceBetweenCities(cities: String*): String =
     DistanceCalculator(cities: _*)(graph) match {
       case Some(distance) => distance.toString
-      case _ => "NO SUCH ROUTE"
+      case _ => NoRouteMessage
+    }
+
+  def shortestPathBetweenCities(startCity: String, endCity: String): String =
+    ShortestPathCalculator(startCity, endCity)(graph) match {
+      case Some(distance) => distance.toString
+      case _ => NoRouteMessage
     }
 }
--- src/test/scala/org/adamkasztenny/trains/TrainCalculationApiIntegrationTest.scala
+++ src/test/scala/org/adamkasztenny/trains/TrainCalculationApiIntegrationTest.scala
@@ -7,15 +7,37 @@ import scalax.collection.immutable.Graph
 
 class TrainCalculationApiIntegrationTest extends FunSuite with Matchers {
 
-  test("returns the distance between two cities on a graph if there is a path between them") {
+  trait ConnectedCities {
     val graph = Graph(WkDiEdge("A", "B")(5), WkDiEdge("B", "C")(4), WkDiEdge("A", "C")(7))
     val api = new TrainCalculationApi(graph)
-    api.distanceBetweenCities("A", "B", "C") shouldBe "9"
   }
 
-  test("returns a no route message if the two cities are not connected") {
+  trait DisconnectedCities {
     val graph = Graph(WkDiEdge("A", "B")(2), WkDiEdge("C", "D")(3))
     val api = new TrainCalculationApi(graph)
-    api.distanceBetweenCities("A", "D") shouldBe "NO SUCH ROUTE"
+  }
+
+  test("returns the distance between two cities on a graph if there is a path between them") {
+    new ConnectedCities {
+      api.distanceBetweenCities("A", "B", "C") shouldBe "9"
+    }
+  }
+
+  test("returns a no route message for distance between cities if the two cities are not connected") {
+    new DisconnectedCities {
+      api.distanceBetweenCities("A", "D") shouldBe api.NoRouteMessage
+    }
+  }
+
+  test("returns the shortest path between two cities on a graph if there is a path between them") {
+    new ConnectedCities {
+      api.shortestPathBetweenCities("A", "C") shouldBe "7"
+    }
+  }
+
+  test("returns a no route message for the shortest path between cities if the two cities are not connected") {
+    new DisconnectedCities {
+      api.shortestPathBetweenCities("A", "D") shouldBe api.NoRouteMessage
+    }
   }
 }
